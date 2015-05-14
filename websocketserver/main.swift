813064b4e7806a68f45a57963a07049bb23b76d7
Ahmad Mushtaq <ahmad.mushtaq@gmail.com>
2015-05-14 08:08:52 +0300

    Initial Commit

---
+++ websocketserver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  websocketserver
+//
+//  Created by Ahmad Mushtaq on 14/05/15.
+//  Copyright (c) 2015 Ahmad Mushtaq. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
